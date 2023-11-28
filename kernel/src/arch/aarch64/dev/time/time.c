12020109dad1e7daf23350aa6993226cd0a11fcb
suhaspai <suhaspai64@gmail.com>
2023-11-28 14:36:14 -0800

    `aarch64/time` Print out physical timer frequency

--- kernel/src/arch/aarch64/dev/time/time.c
+++ kernel/src/arch/aarch64/dev/time/time.c
@@ -3,10 +3,18 @@
  * © suhas pai
  */
 
+#include "dev/printk.h"
 #include "sys/boot.h"
+
+static uint64_t g_frequency = 0;
 
 uint64_t nsec_since_boot() {
     return (uint64_t)boot_get_time();
 }
 
-void arch_init_time() {}
+void arch_init_time() {
+    asm volatile ("mrs %0, cntfrq_el0" : "=r"(g_frequency));
+    printk(LOGLEVEL_INFO,
+           "time: frequency is %" PRIu64 " ticks/ms\n",
+           g_frequency);
+}
