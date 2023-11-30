4f95230b41a83e44020ee9a8f91415d1a40f81dd
suhaspai <suhaspai64@gmail.com>
2023-11-29 19:07:12 -0800

    `lib/list` Add `slist` for single-linked lists

--- lib/list.h
+++ lib/list.h
@@ -8,6 +8,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#include "lib/assert.h"
 #include "macros.h"
 
 // list is a circular doubly-linked list
@@ -16,11 +17,20 @@ struct list {
     struct list *next;
 };
 
+struct slist {
+    struct slist *next;
+};
+
 #define LIST_INIT(name) { .prev = &(name), .next = &(name) }
+#define SLIST_INIT(name) { .prev = &(name) }
 
 __optimize(3) static inline void list_init(struct list *const head) {
     head->prev = head;
     head->next = head;
+}
+
+__optimize(3) static inline void slist_(struct slist *const list) {
+    list->next = list;
 }
 
 __optimize(3) static inline void
@@ -41,6 +51,15 @@ static inline void list_add(struct list *const head, struct list *const item) {
     list_add_common(item, head, head->next);
 }
 
+__optimize(3) static inline void
+slist_add(struct slist *const head,
+          struct slist *const tail,
+          struct slist *const item)
+{
+    tail->next = item;
+    item->next = head;
+}
+
 // Add to back of list
 __optimize(3)
 static inline void list_radd(struct list *const head, struct list *const item) {
@@ -51,6 +70,10 @@ __optimize(3) static inline bool list_empty(struct list *const list) {
     return list == list->prev;
 }
 
+__optimize(3) static inline bool slist_empty(struct slist *const list) {
+    return list == list->next;
+}
+
 __optimize(3) static inline void list_remove(struct list *const elem) {
     elem->next->prev = elem->prev;
     elem->prev->next = elem->next;
@@ -59,12 +82,38 @@ __optimize(3) static inline void list_remove(struct list *const elem) {
     elem->next = elem;
 }
 
+__optimize(3) static inline
+void slist_remove(struct slist *const head, struct slist *const elem) {
+    for (struct slist *iter = head->next; iter != head; iter = iter->next) {
+        if (iter->next == elem) {
+            iter->next = elem->next;
+            break;
+        }
+    }
+
+    verify_not_reached();
+}
+
 __optimize(3) static inline void list_delete(struct list *const elem) {
     elem->next->prev = elem->prev;
     elem->prev->next = elem->next;
 
     elem->prev = NULL;
     elem->next = NULL;
+}
+
+__optimize(3) static inline
+void slist_delete(struct slist *const head, struct slist *const elem) {
+    for (struct slist *iter = head->next; iter != head; iter = iter->next) {
+        if (iter->next == elem) {
+            iter->next = elem->next;
+            elem->next = NULL;
+
+            break;
+        }
+    }
+
+    verify_not_reached();
 }
 
 #define list_rm(type, elem, name) \
@@ -95,6 +144,8 @@ __optimize(3) static inline void list_delete(struct list *const elem) {
     for(iter = list_tail(list, typeof(*iter), name); &iter->name != (list); \
         iter = list_prev(iter, name))
 
+#define slist_foreach(iter, list, name) list_foreach(iter, list, name)
+
 #define list_count(list, type, name) ({  \
     uint64_t __result__ = 0;             \
     type *__iter__ = NULL;               \
@@ -109,6 +160,9 @@ __optimize(3) static inline void list_delete(struct list *const elem) {
              tmp = list_next(iter, name);             \
          &iter->name != (list);                       \
          iter = tmp, tmp = list_next(iter, name))
+
+#define slist_foreach_mut(iter, tmp, list, name) \
+    list_foreach_mut(iter, tmp, list, name)
 
 #define list_foreach_reverse_mut(iter, tmp, list, name) \
     for (iter = list_tail(list, typeof(*iter), name), \
