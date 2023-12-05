fd4ac22d6df16048061d8240eb39eedf68197ac0
suhaspai <suhaspai64@gmail.com>
2023-12-05 00:06:05 -0800

    `lib/alloc` Add `calloc_size()`

--- lib/alloc.h
+++ lib/alloc.h
@@ -11,6 +11,17 @@
 
     #define malloc(size) kmalloc(size)
     #define calloc(amt, size) kmalloc(check_mul_assert((uint32_t)(amt), (size)))
+    #define calloc_size(amt, size, out) ({ \
+        uint32_t __calloc_new_size__ = 0; \
+        uint32_t __calloc_obj_size__ = (size); \
+        __auto_type __calloc_result__ = \
+            kmalloc_size(check_mul_assert((uint32_t)(amt), \
+                                          __calloc_obj_size__), \
+                         &__calloc_new_size__); \
+        *(out) = __calloc_new_size__ / __calloc_obj_size__; \
+        __calloc_result__; \
+    })
+
     #define malloc_size(size, out) kmalloc_size(size, out)
     #define realloc(buffer, size) krealloc(buffer, size)
     #define free(buffer) kfree(buffer)
@@ -20,9 +31,15 @@
         *(out) = (size); \
         malloc(size); \
     })
+    #define calloc_size(amt, size, out) ({ \
+        __auto_type __calloc_amt__ = (amt); \
+        *(out) = __calloc_amt__; \
+        calloc(__calloc_amt__, (size)); \
+    })
 #else
     void *malloc(uint64_t size);
     void *calloc(uint64_t amount, uint64_t size);
+    void *calloc(uint64_t amount, uint64_t size, uint64_t *size_out);
     void *malloc_size(uint64_t size, uint64_t *out);
     void *realloc(void *buffer, uint64_t size);
     void free(void *buffer);
