524adea7d1a6cc61f0ef0579cef8ef0c475a1f50
suhaspai <suhaspai64@gmail.com>
2023-12-06 09:01:58 -0800

    `acpi/fadt` Small style change

--- kernel/src/acpi/fadt.c
+++ kernel/src/acpi/fadt.c
@@ -29,8 +29,7 @@ void fadt_init(const struct acpi_fadt *const fadt) {
         printk(LOGLEVEL_WARN, "fadt: ps2 devices are not supported\n");
     }
 
-    assert_msg(fadt->century != 0,
-               "fadt: rtc century register not supported");
+    assert_msg(fadt->century != 0, "fadt: rtc century reg not supported");
 #elif defined(__aarch64__)
     printk(LOGLEVEL_INFO,
            "fadt: flags: 0x%" PRIx32 "\n",
