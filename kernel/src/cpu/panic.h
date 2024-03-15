1d2a842adb9394d20e557955e809f6f3692b63a5
suhaspai <suhaspai64@gmail.com>
2024-03-14 20:08:03 -0700

    `cpu/panic` Remove unimpled `vpanic()`

--- kernel/src/cpu/panic.h
+++ kernel/src/cpu/panic.h
@@ -4,9 +4,6 @@
  */
 
 #pragma once
-
-#include <stdarg.h>
 #include "lib/macros.h"
 
 __printf_format(1, 2) __noreturn void panic(const char *fmt, ...);
-__noreturn void vpanic(const char *fmt, va_list list);
