e48d5afd1773fb9a94ad9486235c1d1240121984
suhaspai <23438310+suhas-pai@users.noreply.github.com>
2024-07-09 18:45:59 -0700

    `riscv64/mm_types` Remove `__PTE_ACCESSED` flag from leaf ptes
    Flag is no longer required when running in QEMU

--- kernel/src/arch/riscv64/mm/types.h
+++ kernel/src/arch/riscv64/mm/types.h
@@ -114,6 +114,6 @@ enum pte_flags {
 
 #define PGT_FLAGS __PTE_VALID
 #define PTE_LARGE_FLAGS(level) ({ (void)(level); __PTE_VALID; })
-#define PTE_LEAF_FLAGS (__PTE_VALID | __PTE_ACCESSED)
+#define PTE_LEAF_FLAGS __PTE_VALID
 
 struct page;
