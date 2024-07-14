c5c49ee53b74e891d71097547ce26d4bf8cb0dbf
suhaspai <23438310+suhas-pai@users.noreply.github.com>
2024-07-13 22:41:27 -0700

    `lib/math` Change internal var names for macros

--- lib/math.h
+++ lib/math.h
@@ -12,13 +12,13 @@ bool round_up(uint64_t number, uint64_t multiple, uint64_t *result_out);
 bool math_pow(uint64_t base, uint64_t exp, uint64_t *result_out);
 
 #define round_up_assert(number, multiple) ({ \
-    uint64_t h_var(result) = 0; \
-    assert(round_up((number), (multiple), &h_var(result))); \
-    h_var(result); \
+    uint64_t h_var(round_up_result) = 0; \
+    assert(round_up((number), (multiple), &h_var(round_up_result))); \
+    h_var(round_up_result); \
 })
 
 #define math_pow_assert(base, exp) ({ \
-    uint64_t h_var(result) = 0; \
-    assert(math_pow((base), (exp), &h_var(result))); \
-    h_var(result); \
+    uint64_t h_var(math_pow_result) = 0; \
+    assert(math_pow((base), (exp), &h_var(math_pow_result))); \
+    h_var(math_pow_result); \
 })
